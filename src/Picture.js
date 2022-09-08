21bc9e4990c10c1866f570417ad5abccddda8133
Chirchir4 <gloriachirchir4@gmail.com>
2022-09-08 15:10:22 +0300

    Add characteristics of pokemon

--- src/Picture.js
+++ src/Picture.js
@@ -1,33 +1,30 @@
 import React from 'react'
 
-export default function Picture({data}) {
- 
- console.log(data)
- 
- 
- const mapSprites= data.map((item)=>{
-return(
-
-  <div className="card">
-    <div className="name">
-{item.name}
-      
-  
-    </div>
-
-  <img className='img' src={item.sprites.other.home.front_default} alt='' />   
-
-  </div>
-)
- })
-
+export default function Picture({ data }) {
+  console.log(data)
+  const mapSprites = data.map((item) => {
+    return (
+      <div className='cards'>
+        <div className="cardA">
+          <img className='img' src={item.sprites.other.home.front_default} alt='' />
+        </div>
+        <div className="cardB">
+          <div className="name">
+            <ul>Name:{item.name}</ul>
+            <ul>Height:{item.height}</ul>
+            <ul>Ability:{item.abilities.map((item) => {
+              return item.ability.name
+            })}</ul>
+          </div>
+        </div>
+      </div >
+    )
+  })
   return (
-    <div className="picture">   
-
-{
-  mapSprites
-}
-
+    <div className="picture">
+      {
+        mapSprites
+      }
     </div>
   )
 }
